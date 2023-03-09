73901fb7a72332d6b170c8f3c30dc23473749bcd
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-09 17:45:40 -0500

    updating hero banner

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,23 +1,34 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Ball = ({ position, texture }) => {
-  const [decal] = useTexture([texture]);
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.25} rotationIntensity={0.75} floatIntensity={1}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[0, 0, 2]} intensity={0.5} />
-      <mesh castShadow receiveShadow scale={[1.75, 1.75, 1.75]} position={position}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial color='#fff8eb' flatShading />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={0.5}
+          scale={1}
           map={decal}
           flatShading
         />
@@ -31,22 +42,19 @@ const BallCanvas = ({ icon }) => {
     <Canvas
       frameloop='demand'
       dpr={[1, 2]}
-      gl={{ antialias: true, alpha: false, powerPreference: "low-power" }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           autoRotate
-          autoRotateSpeed={10}
-          minDistance={1}
-          maxDistance={3}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 1.75}
+          autoRotateSpeed={20}
+
         />
-        <Ball position={[0, 0, 0]} texture={icon} />
+        <Ball imgUrl={icon} />
       </Suspense>
 
-      <Preload all />
+      {/* <Preload all /> */}
     </Canvas>
   );
 };
