3831cf6c95c0b9f0896504155c5ec26356729e66
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-09 17:09:43 -0500

    updating hero banner

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,26 +14,28 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+    <group>
+      <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+        <ambientLight intensity={0.25} />
+        <directionalLight position={[0, 0, 0.05]} />
+        <mesh castShadow receiveShadow scale={2.75}>
+          <icosahedronGeometry args={[1, 1]} />
+          <meshStandardMaterial
+            color='#fff8eb'
+            polygonOffset
+            polygonOffsetFactor={-5}
+            flatShading
+          />
+          <Decal
+            position={[0, 0, 1]}
+            rotation={[2 * Math.PI, 0, 6.25]}
+            scale={1}
+            map={decal}
+            flatShading
+          />
+        </mesh>
+      </Float>
+    </group>
   );
 };
 
@@ -43,21 +45,12 @@ const BallCanvas = ({ icon }) => {
       frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      onCreated={({ gl }) => {
-        gl.setPixelRatio(window.devicePixelRatio);
-        gl.setSize(window.innerWidth, window.innerHeight);
-      }}
+      camera={{ position: [0, 0, 5] }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          autoRotate
-          autoRotateSpeed={20}
-          maxDistance={10}
-        />
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={20} />
         <Ball imgUrl={icon} />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
