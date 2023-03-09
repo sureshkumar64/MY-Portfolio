5ee4ea8af7beaa2e952614b1aa96584862dade85
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-09 17:28:17 -0500

    updating hero banner

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,55 +1,35 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+const Ball = ({ position, texture }) => {
+  const [decal] = useTexture([texture]);
 
   return (
-    <group>
-      <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-        <ambientLight intensity={0.25} />
-        <directionalLight position={[0, 0, 0.05]} />
-        <mesh castShadow receiveShadow scale={2.75}>
-          <icosahedronGeometry args={[1, 1]} />
-          <meshStandardMaterial
-            color='#fff8eb'
-            polygonOffset
-            polygonOffsetFactor={-5}
-            flatShading
-          />
-          <Decal
-            position={[0, 0, 1]}
-            rotation={[2 * Math.PI, 0, 6.25]}
-            scale={1}
-            map={decal}
-            flatShading
-          />
-        </mesh>
-      </Float>
-    </group>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow position={position} scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} flatShading />
+      </mesh>
+    </Float>
   );
 };
 
 const BallCanvas = ({ icon }) => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const pixelRatio = isMobile ? 0.5 : 1;
+  const shadowMapSize = isMobile ? 256 : 1024;
+
   return (
-    <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [0, 0, 5] }}
-    >
+    <Canvas frameloop='demand' dpr={[1, pixelRatio]} gl={{ preserveDrawingBuffer: true }} shadowMap={{ enabled: true, type: "PCFSoft", soft: true, size: shadowMapSize }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={20} />
-        <Ball imgUrl={icon} />
+        <Ball position={[0, 0, 0]} texture={icon} />
       </Suspense>
       <Preload all />
     </Canvas>
