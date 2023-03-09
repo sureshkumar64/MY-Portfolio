dc850026dff7db0d774df45368b0df2f7e109993
Jeff Jiang <jeff.jiang13@gmail.com>
2023-03-09 16:20:23 -0500

    updating hero banner

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,12 +1,6 @@
 import React, { Suspense } from "react";
-import { Canvas, Html } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
@@ -19,59 +13,32 @@ const Ball = (props) => {
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
+        <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} flatShading />
       </mesh>
     </Float>
   );
 };
 
 const BallCanvas = ({ icon }) => {
-  const [texture] = useTexture([icon]);
-
   return (
-    <>
-      <Canvas
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          zIndex: -1,
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <mesh>
-          <planeBufferGeometry args={[10, 10]} />
-          <meshBasicMaterial map={texture} />
-        </mesh>
-      </Canvas>
-      <Html>
-        <Canvas
-          frameloop='demand'
-          dpr={[1, 2]}
-          gl={{ preserveDrawingBuffer: true }}
-        >
-          <Suspense fallback={<CanvasLoader />}>
-            <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={20} />
-            <Ball imgUrl={icon} />
-          </Suspense>
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{
+        powerPreference: "high-performance",
+        antialias: false,
+        stencil: false,
+        depth: false,
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={20} />
+        <Ball imgUrl={icon} />
+      </Suspense>
 
-          <Preload all />
-        </Canvas>
-      </Html>
-    </>
+      <Preload all />
+    </Canvas>
   );
 };
 
