05443582c4af38ad051cbf903727e83ed9a0919e
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-15 22:25:52 -0400

    fixed background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
 import { TicofabCanvas } from "./canvas";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 
