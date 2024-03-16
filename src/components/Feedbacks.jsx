bbe5db121e3a302a15181d91ac346bfb6a203448
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-15 23:12:45 -0400

    fixed email

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,6 +5,7 @@ import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
+import { ComputersCanvas } from "./canvas";
 
 const FeedbackCard = ({
   index,
@@ -16,19 +17,19 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
   >
-    <p className='text-white font-black text-[48px]'>"</p>
+    <p className="text-white font-black text-[48px]">"</p>
 
-    <div className='mt-1'>
-      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+    <div className="mt-1">
+      <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
 
-      <div className='mt-7 flex justify-between items-center gap-1'>
-        <div className='flex-1 flex flex-col'>
-          <p className='text-white font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span> {name}
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p className="text-white font-medium text-[16px]">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p className='mt-1 text-secondary text-[12px]'>
+          <p className="mt-1 text-secondary text-[12px]">
             {designation} of {company}
           </p>
         </div>
@@ -36,7 +37,7 @@ const FeedbackCard = ({
         <img
           src={image}
           alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
+          className="w-10 h-10 rounded-full object-cover"
         />
       </div>
     </div>
@@ -45,21 +46,37 @@ const FeedbackCard = ({
 
 const Feedbacks = () => {
   return (
-    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+    <>
       <div
-        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+        className="relative w-full h-screen mx-auto"
+        style={{ bottom: "15rem" }}
       >
-        <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={`${styles.sectionHeadText} animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black`}>Testimonials..</h2>
-        </motion.div>
+        <ComputersCanvas />
       </div>
-      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
-        {testimonials.map((testimonial, index) => (
-          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
-        ))}
+      <div className={` bg-black-100 rounded-[20px]`}>
+        <div
+          className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+        >
+          <motion.div variants={textVariant()}>
+            <p className={styles.sectionSubText}>What others say</p>
+            <h2
+              className={`${styles.sectionHeadText} animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black`}
+            >
+              Testimonials..
+            </h2>
+          </motion.div>
+        </div>
+        <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
+          {testimonials.map((testimonial, index) => (
+            <FeedbackCard
+              key={testimonial.name}
+              index={index}
+              {...testimonial}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
