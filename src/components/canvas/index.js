d7ba9efecbb31f27c524c6c5302749ef6e246479
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-15 20:54:27 -0400

    fixed email

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,5 +2,5 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
-import TicofabCanvas from "./tico";
+import TicofabCanvas from "./Tico";
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, TicofabCanvas };
