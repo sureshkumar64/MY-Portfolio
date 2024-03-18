ef40cfa90b3a07afff81fe70ede9ba83173de24a
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-18 00:06:48 -0400

    fixed mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { jeff } from "../assets";
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
-    <motion.div
+    <div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
@@ -31,19 +31,19 @@ const ServiceCard = ({ index, title, icon }) => (
           {title}
         </h3>
       </div>
-    </motion.div>
+    </div>
   </Tilt>
 );
 
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={`${styles.sectionHeadText} animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black`}>Overview.</h2>
-      </motion.div>
+      </div>
       <div className="flex items-center min-[1000px]:flex-row flex-col-reverse">
-        <motion.p
+        <p
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
@@ -66,9 +66,9 @@ const About = () => {
             jeff.jiang13@gmail.com.
           </a>{" "}
           I'm always open to new opportunities and collaborations!{" "}
-        </motion.p>
+        </p>
         <Tilt className="xs:w-[350px] xs:h-[350px] w-full h-full m-auto max-[1000px]:my-14">
-          <motion.div
+          <div
             variants={fadeIn("", "", 0.5, 1)}
             className="xs:w-[350px] w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
           >
@@ -82,7 +82,7 @@ const About = () => {
                 className="w-full h-full object-contain"
               />
             </div>
-          </motion.div>
+          </div>
         </Tilt>
       </div>
       <div className="mt-20 flex flex-wrap gap-10">
