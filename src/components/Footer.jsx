735b8c866ff65be7aae86f8cb0d9feb3728eb50f
Jeff Jiang <jeff.jiang13@gmail.com>
2024-03-19 21:51:09 -0400

    fixed mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { LOGO2 } from "../assets";
 import Typewriter from "react-ts-typewriter";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const [active, setActive] = useState("");
+
   return (
     <div className="bg-black-200 sm:px-16 px-6 sm:py-12 py-10 min-h-[250px] mt-20">
       <div className="flex justify-center items-center">
-        <img src={LOGO2} alt="logo" className="w-28 h-28 object-contain" />
+        <Link
+          to="/"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={LOGO2} alt="logo" className="w-28 h-28 object-contain" />
+        </Link>
         <p className="text-secondary text-[17px] leading-[30px] text-center select-none">
           {new Date().getFullYear()} &copy; Created by Jeff Jiang. All rights
           reserved.
